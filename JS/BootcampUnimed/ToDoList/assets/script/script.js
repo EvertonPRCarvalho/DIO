d7b3d1d65a22d2b04555173846f61f852e45bf09
Everton Carvalho <evertooon03@gmail.com>
2022-07-05 10:22:33 -0300

    To-do List Finalizado

--- JS/BootcampUnimed/ToDoList/assets/script/script.js
+++ JS/BootcampUnimed/ToDoList/assets/script/script.js
@@ -1,9 +1,34 @@
 var x = document.getElementsByClassName("checkbox");
 var y = document.createElement("input.checkbox");
+
+var lista = document.getElementsByClassName("lista_tarefas").innerHTML;
+var seuTextoAqui = "TESTE";
 function newContent(){
-    document.getElementsByClassName("checkbox").appendChild(y);
+    lista = lista + "<li>" + seuTextoAqui +"</li>";
+    document.getElementsByClassName("lista_tarefas").innerHTML = lista;
 }
 function verify(x){
     return x.setAttribute("checked", "checked")
 }
-//temp(x);
+function addWork(){
+
+}
+
+function removeExemplo(){
+    var node = document.getElementById("exemplo"); 
+    if(node.parentNode){ 
+        node.parentNode.removeChild(node);
+    }//<--Essa condição elimina o exemplo que é listado para o usuário.
+}
+
+
+function adicionar() {
+
+    var tarefa = document.getElementById("new-checkbox").value;
+    var lista  = document.getElementById("lista").innerHTML;
+    lista = lista +"<li><input type='checkbox' class='checkbox'>"+tarefa+"</li>";
+  
+    document.getElementById("lista").innerHTML = lista;
+    document.getElementById("new-checkbox").value=''; //Limpa o input após a inserção
+
+  }
